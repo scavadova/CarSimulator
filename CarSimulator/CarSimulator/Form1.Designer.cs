e43c8e55765f0edc0f89ff5862da19dd649381e1
scavadova <scavadova@std.beu.edu.az>
2025-11-05 23:57:17 +0400

    CarSimulator
    C#

---
+++ CarSimulator/CarSimulator/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace CarSimulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            pictureBox9 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            panel4 = new Panel();
+            panel3 = new Panel();
+            panel2 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            label1 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            timer2 = new System.Windows.Forms.Timer(components);
+            label2 = new Label();
+            textBox1 = new TextBox();
+            pictureBox5 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(64, 64, 64);
+            panel1.Controls.Add(pictureBox9);
+            panel1.Controls.Add(pictureBox8);
+            panel1.Controls.Add(pictureBox7);
+            panel1.Controls.Add(pictureBox6);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(panel4);
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(-4, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(368, 530);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.Image = (Image)resources.GetObject("pictureBox9.Image");
+            pictureBox9.Location = new Point(240, 77);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(45, 50);
+            pictureBox9.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox9.TabIndex = 15;
+            pictureBox9.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location = new Point(16, 122);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(45, 50);
+            pictureBox8.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox8.TabIndex = 14;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(260, 204);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(45, 50);
+            pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox7.TabIndex = 13;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(72, 362);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(45, 50);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 12;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(251, 402);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(54, 115);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.White;
+            panel4.Location = new Point(172, 34);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(24, 125);
+            panel4.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.White;
+            panel3.Location = new Point(172, 204);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(24, 125);
+            panel3.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.White;
+            panel2.Location = new Point(172, 374);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(24, 125);
+            panel2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(0, 64, 0);
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(384, 191);
+            button1.Name = "button1";
+            button1.Size = new Size(123, 92);
+            button1.TabIndex = 3;
+            button1.Text = "GAS\r\n";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(64, 0, 0);
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(551, 191);
+            button2.Name = "button2";
+            button2.Size = new Size(123, 92);
+            button2.TabIndex = 4;
+            button2.Text = "BREAK";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(586, 375);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(88, 56);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(384, 375);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(88, 56);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 6;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(499, 429);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(56, 88);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 7;
+            pictureBox4.TabStop = false;
+            pictureBox4.Click += pictureBox4_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Impact", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(482, 324);
+            label1.Name = "label1";
+            label1.Size = new Size(95, 28);
+            label1.TabIndex = 8;
+            label1.Text = "CONTROL";
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // timer2
+            // 
+            timer2.Tick += timer2_Tick;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(551, 62);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 20);
+            label2.TabIndex = 9;
+            label2.Text = "Count:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(608, 59);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(66, 27);
+            textBox1.TabIndex = 10;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(384, 34);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(123, 113);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 11;
+            pictureBox5.TabStop = false;
+            pictureBox5.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(686, 527);
+            Controls.Add(pictureBox5);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Car simulator";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel4;
+        private Panel panel3;
+        private Panel panel2;
+        private Button button1;
+        private Button button2;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer2;
+        private Label label2;
+        private TextBox textBox1;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox7;
+    }
+}
---
+++ CarSimulator/CarSimulator/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CarSimulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+        bool isGameOver = false;
+        bool isCounted1 = false;
+        bool isCounted2 = false;
+        bool isCounted3 = false;
+        bool isCounted4 = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rand1 = new Random();
+            Random rand2 = new Random();
+            Random rand3 = new Random();
+            Random rand4 = new Random();
+            Random rand5 = new Random();
+            Random rand6 = new Random();
+            Random rand7 = new Random();
+            Random rand8 = new Random();
+            textBox1.Text = "0";
+
+            pictureBox6.Location = new Point(rand1.Next(0, 120), rand2.Next(360, 480));
+            pictureBox7.Location = new Point(rand3.Next(200, 320), rand4.Next(200, 280));
+            pictureBox8.Location = new Point(rand5.Next(0, 120), rand6.Next(30, 130));
+            pictureBox9.Location = new Point(rand7.Next(200, 320), rand8.Next(0, 110));
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isGameOver) return;
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (isGameOver) return;
+            timer1.Stop();
+            timer2.Stop();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (isGameOver) return;
+            if (pictureBox1.Location.X < 310)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 20, pictureBox1.Location.Y);
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if (isGameOver) return;
+            if (pictureBox1.Location.X > 0)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X - 20, pictureBox1.Location.Y);
+            }
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            if (isGameOver) return;
+            timer2.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 5);
+
+            if (pictureBox1.Location.Y < 0) timer1.Stop();
+
+            if (pictureBox1.Bounds.IntersectsWith(pictureBox6.Bounds) ||
+                pictureBox1.Bounds.IntersectsWith(pictureBox7.Bounds) ||
+                pictureBox1.Bounds.IntersectsWith(pictureBox8.Bounds) ||
+                pictureBox1.Bounds.IntersectsWith(pictureBox9.Bounds))
+            {
+                timer1.Stop();
+                pictureBox5.Visible = true;
+                isGameOver = true;
+            }
+
+            if (!isCounted1 && pictureBox1.Location.Y < pictureBox6.Location.Y)
+            {
+                count++;
+                textBox1.Text = count.ToString();
+                isCounted1 = true;
+            }
+
+            if (!isCounted2 && pictureBox1.Location.Y < pictureBox7.Location.Y)
+            {
+                count++;
+                textBox1.Text = count.ToString();
+                isCounted2 = true;
+            }
+
+            if (!isCounted3 && pictureBox1.Location.Y < pictureBox8.Location.Y)
+            {
+                count++;
+                textBox1.Text = count.ToString();
+                isCounted3 = true;
+            }
+
+            if (!isCounted4 && pictureBox1.Location.Y < pictureBox9.Location.Y)
+            {
+                count++;
+                textBox1.Text = count.ToString();
+                isCounted4 = true;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 5);
+            if (pictureBox1.Location.Y > 410) timer2.Stop();
+
+            if (pictureBox1.Bounds.IntersectsWith(pictureBox6.Bounds) ||
+                pictureBox1.Bounds.IntersectsWith(pictureBox7.Bounds) ||
+                pictureBox1.Bounds.IntersectsWith(pictureBox8.Bounds) ||
+                pictureBox1.Bounds.IntersectsWith(pictureBox9.Bounds))
+            {
+                timer2.Stop();
+                pictureBox5.Visible = true;
+                isGameOver = true;
+            }
+        }
+
+    }
+}
